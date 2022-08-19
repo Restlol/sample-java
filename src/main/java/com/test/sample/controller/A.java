e5ff4b9bf852b6df3a55ead9d392b09c95dc5cc3
zhaohui <zhaohui@anban.tech>
2022-08-19 12:42:29 +0800

    v3版本，增加对JVM异常的支持

---
+++ src/main/java/com/test/sample/controller/A.java
@@ -0,0 +1,11 @@
+package com.test.sample.controller;
+
+public class A {
+
+    public A() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/test/sample/controller/TestController.java
+++ src/main/java/com/test/sample/controller/TestController.java
@@ -2,25 +2,31 @@ package com.test.sample.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.DispatcherServlet;
+import io.swagger.models.auth.In;
+import org.springframework.web.bind.annotation.*;
+import sun.awt.image.ByteComponentRaster;
+import sun.misc.VM;
 
 import javax.naming.*;
 import javax.naming.directory.*;
 import javax.sql.DataSource;
+import java.Test;
+import java.awt.*;
+import java.awt.image.BandedSampleModel;
+import java.awt.image.Raster;
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -99,10 +105,288 @@ public class TestController {
         Matcher matcher = compile.matcher("hhhhhhhhhh");
     }
 
-    @ApiOperation("error")
-    @GetMapping("error")
-    public void error(String regex)  throws PatternSyntaxException{
-       int a = 1/0;
+    /**
+     *
+     * @param input 为负数就会出现异常
+     */
+    @ApiOperation("Negative Array Size")
+    @GetMapping("negative-array-size")
+    public void negativeArraySize(int input)  {
+        System.out.println("url: [negative-array-size] , param is : " + input);
+        String[] stringArray2 = new String[input];
     }
+
+    /**
+     *
+     * @param dataType
+     * @param w  假设w的输入为：55555555 即会报错
+     * @param h
+     * @param numBands
+     */
+    @ApiOperation("Raster Format Exception")
+    @GetMapping("raster")
+    public void raster(int dataType, int w, int h, int numBands)  {
+        System.out.println("url: [raster] , dataType is : " + dataType + ", w is :" + w + ", h is :" + h + ", numBands is : " + numBands);
+        BandedSampleModel model = new BandedSampleModel(dataType,w,h,numBands);
+        ByteComponentRaster raster =new ByteComponentRaster(model, new Point());
+    }
+
+    /**
+     *
+     * @param input 参数为英文字符串即会报错
+     */
+    @ApiOperation("Class Cast Exception")
+    @GetMapping("classCast")
+    public void classCast(Object input)  {
+        System.out.println("url: [classCaste] , param is : " + input);
+        Integer a = (Integer)input;
+    }
+
+    /**
+     *
+     * @param input 参数大于1即会报错
+     */
+    @ApiOperation("Array Out of Bound")
+    @GetMapping("arrayOutOfBound")
+    public void arrayBound(int input)  {
+        System.out.println("url: [arrayOutOfBound] , param is : " + input);
+        int[] a = {1};
+        int i = a[input];
+    }
+
+    /**
+     *
+     * @param input 参数小于等于0即会报错
+     */
+    @ApiOperation("Illegal Argument Exception")
+    @GetMapping("illegalArgument")
+    public void illegalArgument(int input)  {
+        System.out.println("url: [illegalArgument] , param is : " + input);
+        if(input <= 0){
+            throw new IllegalArgumentException("参数不能小于0");
+        }
+    }
+
+    /**
+     *
+     * @param input 参数为1 或者 true的时候，则会触发Out of memory,需要调整jvm参数：-Xms15M -Xmx15M
+     */
+    @ApiOperation("Out of memory,需要调整jvm参数：-Xms15M -Xmx15M")
+    @GetMapping("outOfMemory")
+    public void outOfMemory(boolean input)  {
+        System.out.println("url: [outOfMemory] , param is : " + input);
+
+        ArrayList list = new ArrayList();
+       while (input){
+           list.add(new Object());
+           System.out.println("创建了");
+       }
+    }
+
+    /**
+     *
+     * @param input 参数为字母字符串即会触发
+     */
+    @ApiOperation("Number Format Exception")
+    @GetMapping("numberFormat")
+    public void numberFormat(String input)  {
+        System.out.println("url: [numberFormat] , param is : " + input);
+        int i = Integer.parseInt(input);
+    }
+
+    /**
+     *
+     * @param input 参数无论输入什么都会触发该异常
+     */
+    @ApiOperation("Security Exception,参数无论输入什么都会触发异常")
+    @GetMapping("security")
+    public void security(String input) throws ClassNotFoundException {
+        System.out.println("url: [security] , param is : " + input + ", 此接口无论输入什么都会触发该异常");
+        Class<?> aClass = Class.forName("java.Test");
+
+    }
+
+    /**
+     *
+     * @param input 参数输入0 或者 false 即触发
+     */
+    @ApiOperation("Dereference of Null Object")
+    @GetMapping("nullObject")
+    public void nullObject(boolean input)  {
+        System.out.println("url: [nullObject] , param is : " + input );
+        String str = null;
+        if (input) {
+            str = "aaa";
+        }
+        str.length();
+    }
+
+    /**
+     *
+     * @param input 参数大于等于3即报错
+     */
+    @ApiOperation("String Index Out Of Bounds Exception")
+    @GetMapping("stringOutBound")
+    public void stringOutBound(int input)  {
+        System.out.println("url: [stringOutBound] , param is : " + input );
+        String s = "abc";
+        s.charAt(input);
+    }
+
+    /**
+     *
+     * @param input 参数无论输入什么都会触发该异常
+     */
+    @ApiOperation("Uncaught Exception,参数无论输入什么都会触发异常")
+    @GetMapping("uncaught")
+    public void uncaught(String input) throws InterruptedException {
+        System.out.println("url: [uncaught] , param is : " + input + ", 此接口无论输入什么都会触发该异常");
+        Thread t = new Thread(() -> {
+            throw new RuntimeException();
+        });
+        t.start();
+        TimeUnit.MILLISECONDS.sleep(10);
+        System.out.println("uncaught end");
+    }
+
+    /**
+     *
+     * @param input 参数的长度小于3，即会报AssertionError ，JVM去加入参数 -ea
+     */
+    @ApiOperation("Failed AssertionJVM,JVM需加入参数 -ea")
+    @GetMapping("failedAssertion")
+    public void failedAssertion(String input)  {
+        System.out.println("url: [failedAssertion] , param is : " + input);
+        assert (input.length()  > 3);
+    }
+
+    /**
+     *
+     * @param input 参数为0 则异常
+     */
+    @ApiOperation("Arithmetic  Exception")
+    @GetMapping("arithmetic")
+    public void arithmetic(int input)  {
+        System.out.println("url: [arithmetic] , param is : " + input);
+        int a = 1 / input;
+    }
+
+
+    private static class User {
+        private String name;
+        private String gender;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name='" + name + '\'' +
+                    ", gender='" + gender + '\'' +
+                    '}';
+        }
+    }
+
+    /**
+     *
+     * @param user 参数无论输入什么都会触发该异常
+     */
+    @ApiOperation("ArrayStore Exception,参数无论输入什么都会触发异常")
+    @PostMapping("arrayStore")
+    public void arrayStore(@RequestBody User user)  {
+        System.out.println("url: [arrayStore] , param is : " + user);
+        Object[] strings = new String[3];
+        strings[0] = user;
+    }
+
+    /**
+     *
+     * @param input 参数无论输入什么都会触发IO Exception的子类异常
+     */
+    @ApiOperation("IOException,将触发IO Exception的子类异常")
+    @GetMapping("io")
+    public void io(String input) throws IOException {
+        System.out.println("url: [io] , param is : " + input);
+        URL url = new URL(input);
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.getContent();
+    }
+
+    /**
+     *
+     * @param input 参数无论输入什么都会触发异常
+     */
+    @ApiOperation("NoSuchMethodException,参数无论输入什么都会触发异常")
+    @GetMapping("noMethod")
+    public void noMethod(String input) throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        System.out.println("url: [noMethod] , param is : " + input);
+        A a = A.class.newInstance();
+        Method handle =  A.class.getMethod("handle", null);
+        handle.invoke(a,null);
+    }
+
+    /**
+     *
+     * @param input 参数无论输入什么都会触发异常
+     */
+    @ApiOperation("FileNotFoundException")
+    @GetMapping("fileNotFound")
+    public void fileNotFound(String input) throws FileNotFoundException {
+        System.out.println("url: [fileNotFound] , param is : " + input);
+        BufferedReader reader = new BufferedReader(new FileReader(new File(input)));
+    }
+
+    /**
+     *
+     * @param input 参数无论输入什么都会触发异常
+     */
+    @ApiOperation("SQLException,参数无论输入什么都会触发异常")
+    @GetMapping("sqlException")
+    public void sqlException(String input) throws SQLException {
+        System.out.println("url: [sqlException] , param is : " + input);
+        throw new SQLException();
+    }
+
+    /**
+     *
+     * @param input 参数无论输入什么都会触发该异常
+     */
+    @ApiOperation("Illegal ThreadState Exception,参数无论输入什么都会触发异常")
+    @GetMapping("illegalThreadState")
+    public void illegalThreadState(String input)  {
+        System.out.println("url: [illegalThreadState] , param is : " + input + ", 此接口无论输入什么都会触发该异常");
+        Thread t = new Thread(() -> {
+            // non-implement
+        });
+        t.start();
+        t.start();
+    }
+
+    /**
+     *
+     * @param input 参数无论输入什么都会触发该异常
+     */
+    @ApiOperation("Illegal MonitorState Exception,参数无论输入什么都会触发异常")
+    @GetMapping("illegalMonitorState")
+    public void illegalMonitorState(String input) throws InterruptedException {
+        System.out.println("url: [illegalMonitorState] , param is : " + input + ", 此接口无论输入什么都会触发该异常");
+        Object o = new Object();
+        o.wait();
+    }
+
 
 }
